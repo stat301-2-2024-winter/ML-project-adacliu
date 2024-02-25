12b604bc55acc0b76b92e70e1744f9e16d28257d
adacliu <90526642+adacliu@users.noreply.github.com>
2024-02-24 21:47:34 -0600

    recipies

---
+++ 2_recipes.R
@@ -0,0 +1,21 @@
+# Setup pre-processing/recipes
+
+# load packages ----
+library(tidyverse)
+library(tidymodels)
+library(here)
+
+# handle common conflicts
+tidymodels_prefer()
+
+# load training data 
+
+# build recipe 1 
+
+# check recipe 1
+
+# build recipe 2
+
+# check recipe 2
+
+# write out recipes 
