5e1672700a7e2fb3392e80842c0ceb04698fae9f
adacliu <90526642+adacliu@users.noreply.github.com>
2024-02-24 21:46:49 -0600

    1

---
+++ 1_initial_setup.R
@@ -0,0 +1,21 @@
+# Initial data checks, data splitting, & data folding
+
+# load packages ----
+library(tidyverse)
+library(tidymodels)
+library(here)
+
+# handle common conflicts
+tidymodels_prefer()
+
+# load data 
+
+# inspecting target variables
+
+# initial split 
+
+# Creating the training data-set 
+
+# Folding the data-set 
+
+# write out data
