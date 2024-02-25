51068042f872079c8ff754cf6be9919ba683374d
adacliu <90526642+adacliu@users.noreply.github.com>
2024-02-24 21:51:44 -0600

    structuree

--- 1_initial_setup.R
+++ 1_initial_setup.R
@@ -9,12 +9,17 @@ library(here)
 tidymodels_prefer()
 
 # load data 
+mushrooms <- read_csv('../data/raw_data/mushrooms.csv', show_col_types = FALSE)
 
 # inspecting target variables
 
 # initial split 
+set.seed(202402)
+data_split <- initial_split(mushrooms, prop = 0.8, strata = 'class')
 
 # Creating the training data-set 
+train.data <- training(data_split)
+test.data <- testing(data_split)
 
 # Folding the data-set 
 
---
+++ 3_fit_vector.R
@@ -0,0 +1,23 @@
+# Define and fit ...
+
+# load packages ----
+library(tidyverse)
+library(tidymodels)
+library(here)
+
+# handle common conflicts
+tidymodels_prefer()
+
+# load training data
+
+# load pre-processing/feature engineering/recipe
+
+# model specifications 
+
+# define workflows
+
+# hyperparameter tuning values 
+
+# fit workflows/models
+
+# write out results (fitted/trained workflows)
