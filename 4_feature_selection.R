c500199d3d4a5bfd920b66c9e2713e0b36d38256
adacliu <90526642+adacliu@users.noreply.github.com>
2024-03-13 16:32:57 -0500

    results

--- 4_feature_selection.R
+++ 4_feature_selection.R
@@ -39,71 +39,68 @@ rec4 <- rec2 %>%
 
 # mew recipe (features from random forest)
 
-# rf_features <- rf_selected_vars %>% 
-#   # select those that gave 80% of effect
-#   filter(cum <= 80) %>% 
-#   pull(variable)
+rf_features <- rf_selected_vars %>% 
+  # select those that gave 80% of effect
+  filter(cum <= 80) %>% 
+  pull(variable)
 
 
-# rec5 <- rec2 %>%
-#   step_select(all_of(rf_features), class)
+rec5 <- rec2 %>%
+  step_select(all_of(rf_features), class)
 
 
-# # fit on resamples
-# metrics= metric_set(accuracy, recall, precision,
-#                     specificity, f_meas, roc_auc)
+# fit on resamples
+metrics= metric_set(accuracy, recall, precision,
+                    specificity, f_meas, roc_auc)
 
 
-# # performance using features selected by Lasso regression
-# lasso_var_select_perf <- list(nb=nb_wf, logistic=logreg_wf, ridge=ridge_wf, 
-#                               d.tree=dt_wf, rf = rf_wf, xgb=boost_wf) %>%
-#   # remove recipes in workflows
-#   map(remove_recipe) %>%
-#   # update with new recipe
-#   map(add_recipe, recipe=rec3) %>%
-#   map(fit_resamples, resamples=cv_fold1, 
-#       control=control_resamples(save_pred = T, save_workflow = T), 
-#       metrics=metrics) %>%
-#   map_df(collect_metrics, .id='model') %>%
-#   select(model, .metric, mean) %>%
-#   pivot_wider(id_cols = model, names_from = .metric, values_from = mean) %>%
-#   arrange(desc(accuracy))
+# performance using features selected by Lasso regression
+lasso_var_select_perf <- list(nb=nb_wf, logistic=logreg_wf, ridge=ridge_wf, 
+                              d.tree=dt_wf, rf = rf_wf, xgb=boost_wf) %>%
+  # remove recipes in workflows
+  map(remove_recipe) %>%
+  # update with new recipe
+  map(add_recipe, recipe=rec3) %>%
+  map(fit_resamples, resamples=cv_fold1, 
+      control=control_resamples(save_pred = T, save_workflow = T), 
+      metrics=metrics) %>%
+  map_df(collect_metrics, .id='model') %>%
+  select(model, .metric, mean) %>%
+  pivot_wider(id_cols = model, names_from = .metric, values_from = mean) %>%
+  arrange(desc(accuracy))
 
 
-# # performance using features selected by decision tree
-# dtree_var_select_perf <- list(nb=nb_wf, logistic=logreg_wf, ridge=ridge_wf, 
-#                               d.tree=dt_wf, rf = rf_wf, xgb=boost_wf) %>%
-#   # remove recipes in workflows
-#   map(remove_recipe) %>%
-#   # update with new recipe
-#   map(add_recipe, recipe=rec4) %>%
-#   map(fit_resamples, resamples=cv_fold1, 
-#       control=control_resamples(save_pred = T, save_workflow = T), 
-#       metrics=metrics) %>%
-#   map_df(collect_metrics, .id='model') %>%
-#   select(model, .metric, mean) %>%
-#   pivot_wider(id_cols = model, names_from = .metric, values_from = mean) %>%
-#   arrange(desc(accuracy))
+# performance using features selected by decision tree
+dtree_var_select_perf <- list(nb=nb_wf, logistic=logreg_wf, ridge=ridge_wf, 
+                              d.tree=dt_wf, rf = rf_wf, xgb=boost_wf) %>%
+  # remove recipes in workflows
+  map(remove_recipe) %>%
+  # update with new recipe
+  map(add_recipe, recipe=rec4) %>%
+  map(fit_resamples, resamples=cv_fold1, 
+      control=control_resamples(save_pred = T, save_workflow = T), 
+      metrics=metrics) %>%
+  map_df(collect_metrics, .id='model') %>%
+  select(model, .metric, mean) %>%
+  pivot_wider(id_cols = model, names_from = .metric, values_from = mean) %>%
+  arrange(desc(accuracy))
 
 
-# # performance using features selected by random forest
-# rf_var_select_perf <- list(nb=nb_wf, logistic=logreg_wf, ridge=ridge_wf, 
-#                            d.tree=dt_wf, rf = rf_wf, xgb=boost_wf) %>%
-#   # remove recipes in workflows
-#   map(remove_recipe) %>%
-#   # update with new recipe
-#   map(add_recipe, recipe=rec5) %>%
-#   map(fit_resamples, resamples=cv_fold1, 
-#       control=control_resamples(save_pred = T, save_workflow = T), 
-#       metrics=metrics) %>%
-#   map_df(collect_metrics, .id='model') %>%
-#   select(model, .metric, mean) %>%
-#   pivot_wider(id_cols = model, names_from = .metric, values_from = mean) %>%
-#   arrange(desc(accuracy))
+# performance using features selected by random forest
+rf_var_select_perf <- list(nb=nb_wf, logistic=logreg_wf, ridge=ridge_wf, 
+                           d.tree=dt_wf, rf = rf_wf, xgb=boost_wf) %>%
+  # remove recipes in workflows
+  map(remove_recipe) %>%
+  # update with new recipe
+  map(add_recipe, recipe=rec5) %>%
+  map(fit_resamples, resamples=cv_fold1, 
+      control=control_resamples(save_pred = T, save_workflow = T), 
+      metrics=metrics) %>%
+  map_df(collect_metrics, .id='model') %>%
+  select(model, .metric, mean) %>%
+  pivot_wider(id_cols = model, names_from = .metric, values_from = mean) %>%
+  arrange(desc(accuracy))
 
-f <- '#https://www.freelancer.com/u/Gozienkwocha#'
-print(f)
-print('Dont submit this file to your school')
 
-# # save
-# save(lasso_var_select_perf, dtree_var_select_perf, rf_var_select_perf, file='results/feature_selection.RData')
+# save
+save(lasso_var_select_perf, dtree_var_select_perf, rf_var_select_perf, file='results/feature_selection.RData')
